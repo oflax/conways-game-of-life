e17486c31b0173aa497a909bee0b656eeed3d0e8
oflaz <65959013+ymfyw@users.noreply.github.com>
2023-01-06 19:13:57 +0300

    works

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,32 @@
 extern crate crossterm;
 
 use crossterm::cursor;
-use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
-use crossterm::style::Print;
+use crossterm::event::{read, Event, KeyCode, KeyEvent};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 
 use colored::Colorize;
 
 use std::io;
+use std::process;
 use std::io::Write;
 
 fn main() {
     let mut gr_size = String::new().to_string();
     let mut stdout = io::stdout();
+    execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0)).unwrap();
 
     io::stdin().read_line(&mut gr_size);
 
     gr_size.pop();
-
+    
     let coor = gr_size.split("x").collect::<Vec<&str>>();
-
+    
     let x = coor[0].parse().unwrap();
     let y = coor[1].parse().unwrap();
-
-    println!("\nGenerated {} grid", gr_size);
-
+    
     let mut vec: Vec<Vec<i8>> = Vec::new();
     let mut xdata: Vec<i8> = Vec::new();
-
+    
     for _i in 0..y {
         for _j in 0..x {
             xdata.push(0);
@@ -36,25 +35,23 @@ fn main() {
         vec.push(xdata.clone());
         xdata.clear();
     }
-
-    let grid_str: String = "".to_owned();
-
+    
     let mut posx = 0;
     let mut posy = 0;
+    
+    let mut gen = 0;
 
-    enable_raw_mode().unwrap();
+    execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0)).unwrap();
+    display(vec.clone(), 0, 0);
+    println!("\nNesil: {}", gen);
 
-    //clearing the screen, going to top left corner and printing welcoming message
-    execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0), Print("")).unwrap();
-
-    //key detection
     loop {
-        execute!(stdout, cursor::MoveTo(0, 0)).unwrap();
+        enable_raw_mode().unwrap();
+        execute!(stdout, cursor::MoveTo(0,0)).unwrap();
+        execute!(stdout, cursor::Hide).unwrap();
 
-        //matching the key
         match read().unwrap() {
             //Event::Key(event) => println!("{:?}", event),
-            //i think this speaks for itself
             Event::Key(KeyEvent {
                 code: KeyCode::Right,
                 ..
@@ -72,24 +69,34 @@ fn main() {
                 ..
             }) => if posy < vec.len()-1 { posy+=1 },
             Event::Key(KeyEvent {
+                code: KeyCode::Char('a'),
+                ..
+            }) => if vec[posy][posx] == 0 { vec[posy][posx] = 1 } else { vec[posy][posx] = 0 },
+            Event::Key(KeyEvent {
                 code: KeyCode::Char(' '),
                 ..
-            }) => vec[posy][posx] = 1,
+            }) => {
+                    execute!(stdout, Clear(ClearType::All), cursor::Hide).unwrap();
+                    disable_raw_mode().unwrap();
+                    vec = simulate(vec);
+                    gen += 1
+            },
             Event::Key(KeyEvent {
-                code: KeyCode::Char('s'),
+                code: KeyCode::Char('q'),
                 ..
-            }) => vec = simulate(vec, "".to_owned()),
+            }) => process::exit(1),
             _ => (),
         }
-        println!("{:?}\n{:?}", (posx, posy), vec);
+        disable_raw_mode().unwrap();
+        display(vec.clone(), posx, posy);
+        println!("\nNesil: {}", gen);
     }
 }
 
-fn simulate(vec: Vec<Vec<i8>>, mut grid_str: String) -> Vec<Vec<i8>> {
+fn simulate(vec: Vec<Vec<i8>>) -> Vec<Vec<i8>> {
     let mut next = vec.clone();
     for (i, row) in vec.iter().enumerate() {
         for (j, node) in row.iter().enumerate() {
-            // ? let node = if num != x - 1 { "0-" } else { "0" }; // FOR SEPERATOR
             let (mut u, mut ul, mut ur, mut l, mut r, mut b, mut bl, mut br) =
                 (0, 0, 0, 0, 0, 0, 0, 0);
             if i > 0 {
@@ -125,23 +132,23 @@ fn simulate(vec: Vec<Vec<i8>>, mut grid_str: String) -> Vec<Vec<i8>> {
                 next[i][j] = 0;
             }
         }
-        grid_str.push_str("\n");
     }
-    vec
+    next
 }
 
-fn display(vec: Vec<Vec<i8>>, mut grid_str: String) -> String {
-    for row in vec {
-        for item in row {
-            let sym = if item == 1 {
-                "⬢ ".blue()
+fn display(vec: Vec<Vec<i8>>, px: usize, py: usize) {
+    for (y, row) in vec.iter().enumerate() {
+        for (x, item) in row.iter().enumerate() {
+            let sym = if x == px && y == py {
+                // item.to_string().yellow()
+                if *item == 1 { "⬢".yellow() } else { "⬡".yellow() }
+            } else if *item == 1 {
+                "⬢".blue()
             } else {
-                "⬢ ".red()
+                "⬡".red()
             };
-            grid_str.push_str(&sym);
+            print!("{} ", sym);
         }
-        grid_str.push_str("\n");
+        println!();
     }
-    grid_str
 }
-
